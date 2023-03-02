d6a996ae6c704adb907c09228bc42c1e631030cb
Rajab Karume <rkarume@gmail.com>
2023-03-02 11:15:25 +0300

    ✨added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tenants, only: [:show, :delete, :create, :index, :update], defaults: { format: :json }
+  resources :landlords, only: [:show, :delete, :create, :index, :update], defaults: { format: :json }
+  resources :apartments, only: [:show, :delete, :create, :index, :update], defaults: { format: :json }
 end
